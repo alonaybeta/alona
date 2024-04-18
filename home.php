37e259dced99eb1c5903b1f76a1ee094abfbd5bc
alonaybeta <167389228+alonaybeta@users.noreply.github.com>
2024-04-18 10:35:35 +0800

    Add files via upload

---
+++ home.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<style>
+    h1{
+        text-align: center;
+        font-size: 10em !important;
+    }
+    body {font-family: Arial, Helvetica, sans-serif;}
+
+.navbar {
+  width: 100%;
+  background-color: #ED872D;
+  overflow: auto;
+}
+.avatar {
+  
+  width: 1500px;
+  height: 600px;
+  border-radius: 0%;
+}
+
+.navbar a {
+  float: left;
+  padding: 12px;
+  color: green;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.navbar a:hover {
+  background-color: #ED872D;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+
+@media screen and (max-width: 500px) {
+  .navbar a {
+    float: none;
+    display: block;
+  }
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #04AA6D;
+  color: blue;
+}
+
+.header a.active {
+  background-color: blue;
+  color: yellow;
+}
+
+.header-right {
+  float: right;
+}
+</style>
+<body>
+    <?php include 'navbar.php' ?>
+</nav>
+<br><h2><center>WELCOME TO DANAO TECHNOLOGICAL COLLEGE</h2></center></br>
+<br>
+<h2><center>PLEASE VISIT OUR PAGE</h2></center>
+</br>
+<br>
+<h2><center>Gmail:danaotechnologicalcollege072gtmail.com</h2></center>
+</br>
+<h2><center>Courses Offered:</h2><center>
+<h2><center>Bachelor of Science Information system</h2></center>
+<h2><center>Bachelor of Science Tourism managment</h2></center>
+<h2><center>Bachelor of Technical Vocational Education</h2></center>
+<h2><center>(Major of Food Service Management)</h2></center>
+<h2><center>Bachelor of Science in Criminology</h2></center>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+    
+</head>
+<?php include "navbar.php";
+?>
+<style>
+    body{
+        background-image: url('t.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: 100% 100%;
+    }
+    .avatar {
+  
+  width: 1500px;
+  height: 100px;
+  border-radius: 0%;
+}
+    </style>
+<body>
+<?php include 'Login.php' ?>
+
+
+</body>
+</html>
